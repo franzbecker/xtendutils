a8f678aab4e22d5ba2ba9500f4662c6bc82ce592
Franz Becker <becker.franz@gmail.com>
2015-05-22 22:30:39 +0200

    Added "lib" project and first draft of PatternExtensions

---
+++ de.xtendutils.lib/src/main/xtend/de/xtendutils/lib/extensions/PatternExtensions.java
@@ -0,0 +1,88 @@
+package de.xtendutils.lib.extensions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.annotation.Nullable;
+import javax.annotation.ParametersAreNonnullByDefault;
+
+import org.eclipse.xtext.xbase.lib.Inline;
+import org.eclipse.xtext.xbase.lib.Pure;
+
+/**
+ * This is an extension library for {@link Pattern}.
+ */
+@ParametersAreNonnullByDefault
+public class PatternExtensions {
+
+	/**
+	 * Returns a compiled {@link Pattern} with the given regex.
+	 * In contrast to {@link Pattern#compile(String)} passing {@link CharSequence} is supported.
+	 * 
+	 * @param regex a {@link CharSequence}, may not be {@code null}.
+	 * @return the compiled {@link Pattern}.
+	 */
+	@Pure
+	@Inline(value="Pattern.compile($1)", imported=Pattern.class)
+	public static Pattern compile(CharSequence regex) {
+		return Pattern.compile(regex.toString());
+	}
+	
+	/**
+	 * Returns a compiled {@link Pattern} with the given regex and flags.
+	 * In contrast to {@link Pattern#compile(String, int)} passing {@link CharSequence} is supported.
+	 * 
+	 * @param regex a {@link CharSequence}, may not be {@code null}.
+	 * @param flags see {@link Pattern#compile(String, int)}
+	 * @return the compiled {@link Pattern}.
+	 */
+	@Pure
+	@Inline(value="Pattern.compile($1, $2)", imported=Pattern.class)
+	public static Pattern compile(CharSequence regex, int flags) {
+		return Pattern.compile(regex.toString(), flags);
+	}
+	
+	/**
+	 * Returns {@code true} if the pattern matches against the input, {@code false} if not
+	 * or if the input was {@code null}.
+	 * <p/>
+	 * Method is null-safe for the passed character sequence, pattern may not be {@code null}.
+	 * 
+	 * @param input the character sequence to be matched, may be {@code null}.
+	 * @param pattern the pattern to be matched against, may not be {@code null}. 
+	 * @return {@code true} if the pattern matches against the input, {@code false} if not
+	 * 			or if the input was {@code null}.
+	 */
+	public static boolean matches(@Nullable CharSequence input, Pattern pattern) {
+		if (input == null) {
+			return false;
+		}
+		Matcher matcher = pattern.matcher(input);
+		return matcher.find();
+	}
+	
+	/**
+	 * Returns {@code true} if any of the given patterns matches against the input, {@code false}
+	 * if none matches, the input was {@code null} or the list patterns was empty.
+	 * <p/>
+	 * Method is null-safe for the passed character sequence, pattern may not be {@code null}.
+	 * 
+	 * @param input the character sequence to be matched, may be {@code null}.
+	 * @param patterns the pattern to be matched against, may not be {@code null}. 
+	 * @return {@code true} if any of the given patterns matches against the input, {@code false}
+	 * 			if none matches, the input was {@code null} or the list patterns was empty.
+	 */
+	public static boolean matchesAny(@Nullable CharSequence input, Pattern... patterns) {
+		if (input == null) {
+			return false;
+		}
+		for (Pattern pattern : patterns) {
+			Matcher matcher = pattern.matcher(input);
+			if (matcher.find()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+}
---
+++ de.xtendutils.lib/src/test/xtend/de/xtendutils/lib/PatternExtensionsTest.xtend
@@ -0,0 +1,55 @@
+package de.xtendutils.lib
+
+import de.xtendutils.junit.AssertionHelper
+import de.xtendutils.lib.extensions.PatternExtensions
+import org.junit.Test
+
+import static java.util.regex.Pattern.CASE_INSENSITIVE
+
+import static extension de.xtendutils.lib.extensions.PatternExtensions.*
+
+/**
+ * Tests for {@link PatternExtensions}.
+ */
+class PatternExtensionsTest {
+	
+	extension AssertionHelper = AssertionHelper.instance
+	
+	@Test
+	def void testCompile() {
+		// When
+		val wsPattern = "\\s".compile
+		val xPattern = "x".compile(CASE_INSENSITIVE) 
+		
+		// Then
+		wsPattern.pattern.assertEquals("\\s")
+		xPattern.pattern.assertEquals("x")
+		xPattern.flags.assertEquals(CASE_INSENSITIVE)
+	}
+	
+	@Test
+	def void testMatches() {
+		// Given
+		val pattern = "\\s".compile
+		
+		// When + Then
+		" ".matches(pattern).assertTrue
+		"x".matches(pattern).assertFalse
+		null.matches(pattern).assertFalse
+	}
+	
+	@Test
+	def void testMatchesAny() {
+		// Given
+		val patterns = #["\\s".compile, "x".compile(CASE_INSENSITIVE)]
+		val emptyPatterns = #[]
+		
+		// When + Then
+		"x".matchesAny().assertFalse
+		"x".matchesAny(emptyPatterns).assertFalse
+		"x".matchesAny(patterns).assertTrue
+		"X".matchesAny(patterns).assertTrue
+		null.matchesAny(patterns).assertFalse
+	}
+	
+}
